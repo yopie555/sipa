bfb7e078960b056238416b07d63b9ebf445b96f6
yopie555 <yoepytribuana@gmail.com>
2022-01-11 11:00:20 +0700

    initialize sipa project

---
+++ App.js
@@ -0,0 +1,28 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+      <Text>asda</Text>
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
